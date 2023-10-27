b63da04c4517ca5dbed7a2fd9632d332fe914156
omar assidi <omar.assidi@cmeoffshore.com>
2023-10-27 13:27:29 +0300

    added project to git

---
+++ OmarAssidiCard/OmarAssidiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmarAssidiCardApp.swift
+//  OmarAssidiCard
+//
+//  Created by OmarAssidi on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct OmarAssidiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
